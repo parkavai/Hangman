86a41a2f1d902c9382ccb12fa5d2385841ea55fd
Parkavan Illavalagan <86165957+parkavai@users.noreply.github.com>
2022-07-15 23:42:56 +0200

    Implemented necessary functionalitites

--- project/script.js
+++ project/script.js
@@ -1,19 +1,36 @@
-let attempts = 9
+let attempts = 9;
+let guessedCorrect = 0;
+let guessedWrong = 0;
 
-const attemptsText = document.getElementById("attempts_txt")
+const alphabetBox = document.getElementById("alphabet_box");
+const attemptsText = document.getElementById("attempts_text");
+const statusText = document.getElementById("status_text");
 const guessBox = document.getElementById("guess_box");
 const words = ["AWAIT", "HELLO", "SCHOOL"];
-console.log(attemptsText);
-var array = []
+const buttons = [];
+const chosenWord = words[Math.floor(Math.random() * words.length)]
+let array = [];
 
 /**
  * When the window is shown to the user, we want a random word to be chosen in order to 
  * setup the game. 
  */
-const start = () =>{
-    let index = Math.floor(Math.random() * words.length);
-    const chosenWord = words[index];
+const game = () =>{
+    createButtons();
     fillInUnderscores(chosenWord);
+    buttons.forEach(button => button.addEventListener('click', guessWord));
+};
+
+const gameOver = (status) =>{
+    for (var i = 0; i < buttons.length; i++){
+        buttons[i].disabled = true;
+    }
+    if(status = "win"){
+        statusText.innerHTML = "You win!!";
+    }
+    else{
+        statusText.innerHTML = `You Lose, the word was ${chosenWord}`;
+    }
 }
 
 /**
@@ -22,12 +39,54 @@ const start = () =>{
  */
 const fillInUnderscores = (word) =>{
     for (var i = 0; i < word.length; i++){
-        guess = document.createElement("h1");
-        guess.appendChild(document.createTextNode("_"));
+        guess = document.createElement("div");
         guess.classList.add("guess");
         array[i] = guess;
         guessBox.appendChild(guess);
     }
-}
+};
 
-start();
+/**
+ * 
+ */
+const guessWord = (e) =>{
+    letter = e.target.id; 
+    e.target.disabled = true;
+    for(var i = 0; i < chosenWord.length; i++){
+        if(letter == chosenWord[i]){
+            fillInnLetter(i, letter);
+            console.log("Funnet bokstav: " + letter);
+            guessedCorrect += 1;
+        }
+        else{
+            guessedWrong += 1;
+        }
+        attempts -= 1;
+    }
+    if(guessedCorrect == chosenWord.length){
+        console.log("Vunnet");
+        gameOver("win");
+    }
+};
+
+const fillInnLetter = (index, letter) =>{
+    var text = document.createElement("h1");
+    text.classList.add("center");
+    text.textContent = letter;
+    array[index].appendChild(text);
+    console.log(array[index])
+};
+
+const createButtons = () =>{
+    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for(var i = 0; i < alphabet.length; i++){
+        var btn = document.createElement("button");
+        btn.id = alphabet[i];
+        btn.classList.add("alphabet");
+        btn.textContent = alphabet[i];
+        buttons[i] = btn;
+        alphabetBox.appendChild(btn);
+    }
+};
+
+game();
